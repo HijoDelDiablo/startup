015f10eaecb252f3d864f057634596bdbb26c079
HijoDelDiablo <brunoj_22@hotmail.com>
2015-09-29 13:05:29 -0300

    Muestra la lista

--- 05-angularjs/indexcontroller.js
+++ 05-angularjs/indexcontroller.js
@@ -1,3 +1,21 @@
 app.controller('indexcontroller', ['$scope', function($scope){
-	$scope.title = 'The Best Movies';
+	$scope.title = 'My Favourite Movies';
+    $scope.movies = [
+  	{ 
+    	name: 'V for Vendetta', 
+  	},
+
+  	{ 
+    	name: 'Madagascar', 
+  	},
+
+  	{
+  		name: 'Tears Of The Sun',
+  	},
+
+  	{
+  		name: 'Hero',
+  	},
+
+  	]; 
 }]);
